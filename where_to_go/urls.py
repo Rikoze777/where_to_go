2902a528e3c12ecdd50fcd6521b96be10efcd27a
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-11-29 18:13:32 +0300

    Add blank start page

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from map.views import startpage
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', startpage)
 ]

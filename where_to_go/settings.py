977f385c1ecaf9c28e18055779b9f296095addcb
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-12-01 17:02:03 +0300

    fix app name and make env settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from environs import Env
 
+
+env = Env()
+env.read_env()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +24,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+zbffufb53#ize3p4i+93(%gt=8f61c)4d3-bq8dtuuq)2p6u*'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'place.app.PlaceConfig'
 ]
 
 MIDDLEWARE = [
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from map.views import startpage
+from place.views import startpage
 
 
 urlpatterns = [

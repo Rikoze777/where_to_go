74cfc2f613eecdbe6ddd10d75485abc336342f56
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2023-01-19 23:42:12 +0300

    showed images in admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from places.models import Place, Image
+from django.utils.safestring import mark_safe
 
 
 class ImageInline(admin.TabularInline):
     model = Image
     raw_id_fields = ('place', )
+    readonly_fields = ["place_image"]
+
+    def place_image(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url = obj.img.url,
+            width=obj.img.width,
+            height=obj.img.height,
+            )
+    )
 
 
 @admin.register(Place)
@@ -16,5 +26,5 @@ class PlaceAdmin(admin.ModelAdmin):
 
 
 @admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
+class ImageAdmin(admin.ModelAdmin,):
     list_display = ['img']

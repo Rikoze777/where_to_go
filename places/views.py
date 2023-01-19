81026b69afcd62009d8c9fcec5649e062d5c0848
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2023-01-19 20:12:13 +0300

    geo info from db

--- places/views.py
+++ places/views.py
@@ -1,35 +1,31 @@
 from django.shortcuts import render
+from .models import Place
 
 
 def index(request):
+    features = []
+    detailsUrl = ["static/places/moscow_legends.json",
+                  "/static/places/roofs24.json"]
+    places = Place.objects.all()
+    for place in places:
+        feature = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.lng, place.lat]
+                },
+                "properties": {
+                    "title": place.description_short,
+                    "placeId": place.id,
+                    "detailsUrl": detailsUrl[place.id-1]
+                }
+                }
+        features.append(feature)
+
     context = {'places': {
-         "type": "FeatureCollection",
-         "features": [
-            {
-             "type": "Feature",
-             "geometry": {
-                "type": "Point",
-                "coordinates": [37.62, 55.793676]
-             },
-             "properties": {
-                "title": "«Легенды Москвы",
-                "placeId": "moscow_legends",
-                "detailsUrl": "static/places/moscow_legends.json"
-             }
-            },
-            {
-             "type": "Feature",
-             "geometry": {
-                "type": "Point",
-                "coordinates": [37.64, 55.753676]
-             },
-             "properties": {
-                "title": "Крыши24.рф",
-                "placeId": "roofs24",
-                "detailsUrl": "/static/places/roofs24.json"
-             }
-            }
-         ]
-        }
-    }
+                "type": "FeatureCollection",
+                "features": features
+                }
+               }
+
     return render(request, "index.html", context=context)

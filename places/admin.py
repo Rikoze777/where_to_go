7bc71cba907c3a8a17f63bffdf821578fc8d75b0
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2023-01-25 15:21:51 +0300

    Used max-height, max-width

--- places/admin.py
+++ places/admin.py
@@ -11,8 +11,8 @@ class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     readonly_fields = ["place_image", ]
 
     def place_image(self, obj):
-        return format_html("<img src={} height={}/>",
-                           obj.image.url, "200px")
+        return format_html("<img src={} max-height={} max-width={}/>",
+                           obj.image.url, "200px", "200px")
 
 
 @admin.register(Place)
